cdae56d871a7a766a23efa91e4d5a1260d4abfea
Willian <willianandrade953@gmail.com>
2021-12-30 23:07:04 -0200

    post created

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -3,18 +3,24 @@ const router = express.Router();
 const User = require("../../database/models/model");
 
 router.get("/users/all", async (req, res) => {
+    const users = User.find();
 
+    if (!users) 
+
+    res.send({ users: users }).status(200);
 });
 
 router.post("/users/new", async (req, res) => {
     const existingUser = await User.findOne({ id: req.body.id });
 
-    if (existingUser) res.send({ error: "User Aleights exist!" });
+    if (existingUser) res.send({ error: "User Aleights exist!" }).status(404);
 
     const user = new User(req.body);
 
     //Saving in Database
     await user.save();
+
+    res.send({ user: user }).status(200);
 });
 
 router.put("/users/update", (req, res) => {
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 require("./api/controller/controller")(app);
