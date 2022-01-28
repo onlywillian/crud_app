d42b4284519c99f3fcab12250f0c5d0ee85166ad
Willian <willianandrade953@gmail.com>
2022-01-28 12:32:41 -0200

    backend completed

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -11,9 +11,9 @@ router.get("/users/all", async (req, res) => {
 });
 
 router.post("/users/new", async (req, res) => {
-    const existingUser = await User.findOne({ id: req.body._id });
+    const existingUser = await User.findOne({ email: req.body.email });
 
-    if (existingUser) return res.send({ error: "User Aleights exist!" }).status(404);
+    if (existingUser) return res.send({ error: "User already exist!" }).status(404);
 
     const user = new User(req.body);
 
@@ -24,22 +24,26 @@ router.post("/users/new", async (req, res) => {
 });
 
 router.put("/users/update", async (req, res) => {
-    const existingUser = await User.findOne({ id: req.body._id });
+    const existingUser = await User.findOne({ _id: req.body.id });
 
     if (!existingUser) return res.send({ error: "User don't exist's" }).status(404);
 
-    User.updateOne({ id: req.body._id }, { name: req.body.newName });
+    if (!req.body.newEmail) return res.send({ error: "No email" });
 
-    return res.send({ success: "Update completed" }).status(200);
+    User.updateOne({ _id: req.body.id }, { email: req.body.newEmail }, function (err) {
+        if (err) return console.log(err);
+    });
+
+    return res.send({ sucess: "Update completed" }).status(200);
 });
 
-router.delete("users/del", async (req, res) => {
-    const existingUser = await User.findOne({ id: req.body._id });
+router.delete("/users/del", async (req, res) => {
+    const existingUser = await User.findOne({ _id: req.body.id });
 
     if (!existingUser) return res.send({ error: "User don't exist's" }).status(404);
 
     User.deleteOne({ id: req.body._id }, err => {
-        if (err) res.send({ error: "Complex error" })
+        if (err) return res.send({ error: err });
     });
 
     return res.send({ sucess: "User deleted" }).status(200);
